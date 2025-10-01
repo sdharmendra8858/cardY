8c5fa0fa394bbff51a1aa7ea5db501338c3b2946
DHARMENDRA VERMA <sdharmendra8858@gmail.com>
2025-10-01 23:24:08 +0530

    Refactor app structure: remove modal and explore screens, add add-card and card-details screens, update HomeScreen to display card list, and adjust navigation. Update package.json dependencies.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,10 +24,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="add-card"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Add Card',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="creditcard" color={color} />,
         }}
       />
     </Tabs>
